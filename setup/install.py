aa73d10ae46aa414da24b94c56713ec6105a63f4
Adam1000 <azeiser08@outlook.com>
2024-02-09 08:48:13 -0500

    updated stupid things

--- setup/install.py
+++ setup/install.py
@@ -25,17 +25,17 @@ password_store = os.path.join(user_data_path, password_data)
 
 total_iterations = 100
 
-def progress_bar(iteration, total, prefix='', suffix='', length=50, print_end='\r'):
+def progress_bar(iteration, total, prefix="", suffix="", length=50, print_end="\r"):
     percent = ("{0:.1f}").format(100 * (iteration / float(total)))
     filled_length = int(length * iteration // total)
-    bar = '#' * filled_length + '-' * (length - filled_length)
-    sys.stdout.write(f'\r{prefix} [{bar}] {percent}% {suffix}',)
+    bar = "#" * filled_length + "-" * (length - filled_length)
+    sys.stdout.write(f"\r{prefix} [{bar}] {percent}% {suffix}",)
     sys.stdout.flush()
     if iteration == total:
         print(print_end)
 
 def clear_screen():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system("cls" if os.name == "nt" else "clear")
 
 clear_screen()
 
@@ -63,7 +63,7 @@ f.close()
 
 for i in range(total_iterations + 1):
     sleep(0.04)
-    progress_bar(i, total_iterations, prefix='Progress:', suffix='Complete', length=50)
+    progress_bar(i, total_iterations, prefix="Progress", suffix="Complete", length=50)
 
 print("SynOS has successfully installed")
 sleep(0.8)
